ac5411807d66941c79d4d296914074891df45384
Kendrew18 <kendrewtandio71@gmail.com>
2024-10-21 00:27:31 +0700

    template signup

---
+++ controller/user_app/USER.go
@@ -0,0 +1,29 @@
+package user_app
+
+import (
+	"APP-TOKO/service/user_app"
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func SignUp(c echo.Context) error {
+	var Request request.Sign_Up_Request
+
+	err := c.Bind(&Request)
+
+	fmt.Println(Request)
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
+
+	result, err := user_app.Sign_Up(Request)
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
+
+	return c.JSON(result.Status, result)
+}
---
+++ service/cabang/cabang.go
@@ -0,0 +1,10 @@
+package cabang
+
+import "APP-TOKO/model/response"
+
+func Input_Cabang() (response.Response, error) {
+
+	var res response.Response
+
+	return res, nil
+}
